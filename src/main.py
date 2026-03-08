6b1d0fad10b46694d17093732753c74afe13fa7d
krishagrawal30 <agrawalkrishn30@gmail.com>
2026-03-08 06:08:01 +0000

    Visual modifications in main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,32 @@
 import joblib
 from ner import extract_entities
 from executor import execute
+from rich.console import Console
+from rich.panel import Panel
+
+console = Console()
+
 model = joblib.load("models/intent_model.pkl")
 vectorizer = joblib.load("models/vectorizer.pkl")
 
-print("Desktop Assistant NLP")
-print()
-text = input("Enter command: ")
-X = vectorizer.transform([text])
-intent = model.predict(X)[0]
-print("Predicted Intent:", intent)
-entities = extract_entities(text, intent)
-print("Extracted Entities:", entities)
+console.print(Panel.fit("Desktop Assistant ", style="bold cyan"))
+console.print("[dim]Type 'exit' to quit[/dim]\n")
 
-execute(intent, entities)
+while True:
+
+    text = console.input("[bold green]Enter command:[/bold green] ")
+
+    if text.lower() in ["exit", "quit"]:
+        console.print("[bold red]Exiting assistant...[/bold red]")
+        break
+
+    X = vectorizer.transform([text])
+    intent = model.predict(X)[0]
+
+    console.print(f"[yellow]Predicted Intent:[/yellow] {intent}")
+
+    entities = extract_entities(text, intent)
+
+    console.print(f"[magenta]Extracted Entities:[/magenta] {entities}")
+
+    execute(intent, entities)
